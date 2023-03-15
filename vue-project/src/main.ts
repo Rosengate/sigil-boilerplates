45f24d87d63e1d5da94b93c1131dda580933a1de
Ahmad Rahimie <newrehmi@gmail.com>
2023-03-16 00:09:19 +0800

    import components from my other project

--- vue-project/src/main.ts
+++ vue-project/src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import 'bulma'
 
 const app = createApp(App)
 
--- vue-project/src/views/IndexPage.vue
+++ vue-project/src/views/IndexPage.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
+import UtilsContainer from "@/views/Utils/UtilsContainer.vue";
 </script>
-
 <template>
   <main>
   </main>
